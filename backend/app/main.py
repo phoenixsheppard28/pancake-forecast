4339039b368f765217f2b72be4e242e9c4363529
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-04-10 18:54:58 -0400

    time check

--- backend/app/main.py
+++ backend/app/main.py
@@ -49,7 +49,7 @@ async def get_forecast(x_api_key:str = Header(...)):
     for i in range(8):
         today = datetime.today() + timedelta(i)
         formatted_date = today.strftime("%Y-%m-%d")
-
+        print("time??")
         for hall in DINING_HALLS:
             params={
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
