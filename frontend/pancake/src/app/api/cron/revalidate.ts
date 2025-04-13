2a0e907dca8b585a806c7ce3744494a6d2743e41
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-04-12 21:49:15 -0400

    cron job

---
+++ frontend/pancake/src/app/api/cron/revalidate.ts
@@ -0,0 +1,42 @@
+// /pages/api/cron/revalidate.ts
+import { NextResponse,NextRequest } from 'next/server'
+
+
+export async function GET(request:NextRequest) {
+    try {
+  
+        const token = request.headers.get('Authorization');
+        if (!token || token !== `Bearer ${process.env.CRON_SECRET}`) {
+            return NextResponse.json(
+                { message: 'Unauthorized' },
+                { status: 401 }
+            );
+        }
+
+
+        const response = await fetch("/api/pancake-data",{
+            headers: {
+                "x-api-key": process.env.API_KEY || ""
+            },
+            cache: 'no-store', // Force fresh data,
+            next: { revalidate: 0 } // Force revalidation,
+
+    });
+       
+        
+        const revalidatePancake = await response.json();
+  
+        return NextResponse.json({ revalidted: true, revalidatePancake, timestamp: new Date().toISOString(),
+        })
+    } catch (error) {
+        console.error("Error fetching pancake data:", error);
+        return NextResponse.json({
+            revalidated: false,
+            error: error instanceof Error ? error.message : 'Unknown error',
+            timestamp: new Date().toISOString()
+        }, { 
+            status: 500 
+        })
+      }
+}
+
